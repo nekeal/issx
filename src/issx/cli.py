12b575136d8b55c4f78da285b03dc26dd7cfa09f
Szymon Cader <szymon.sc.cader@gmail.com>
2024-04-12 23:29:06 +0200

    feat: Make use of instance manager in auth-verify command

--- src/issx/cli.py
+++ src/issx/cli.py
@@ -6,15 +6,20 @@ from gitlab import Gitlab
 from rich.console import Console
 
 from issx.clients import SupportedBackend
-from issx.clients.gitlab import GitlabClient
+from issx.clients.gitlab import GitlabClient, GitlabInstanceClient
+from issx.clients.redmine import RedmineInstanceClient
+from issx.instance_managers import GenericConfigParser, InstanceManager
 from issx.services import CopyIssueService
 
 app = typer.Typer(no_args_is_help=True)
 
 BackendOption = Annotated[SupportedBackend, typer.Option()]
-
+InstanceNameOption = Annotated[str, typer.Option("--instance")]
 
 console = Console()
+
+InstanceManager.register_backend(SupportedBackend.gitlab, GitlabInstanceClient)
+InstanceManager.register_backend(SupportedBackend.redmine, RedmineInstanceClient)
 
 
 @app.command()
@@ -40,24 +45,31 @@ def copy(
 
 
 @app.command()
-def auth_verify(backend_name: BackendOption = SupportedBackend.gitlab) -> None:
+def auth_verify(instance_name: InstanceNameOption) -> None:
+    config = GenericConfigParser()
+    instance_manager = InstanceManager(config)
     try:
-        gl = Gitlab.from_config()
+        instance = instance_manager.get_instance_client(instance_name)
     except Exception as e:
-        console.print(f"Error when reading config file.\n" f"Details: {e}", style="red")
+        console.print_exception()
+        console.print("Error when configuring client instance.\n", style="red")
         raise typer.Exit(1) from e
     try:
-        gl.auth()
+        username = asyncio.run(instance.auth())
+        if not username:
+            raise Exception("Authentication failed")
     except Exception as e:
+        console.print_exception()
         console.print(
-            f"Error when authenticating to {gl.url}.\nDetails: {e}", style="red"
+            f"Error when authenticating to {instance.get_instance_url()}",
+            style="red",
         )
         raise typer.Exit(1) from e
     else:
         console.print(
             "Authentication successful",
-            f"Instance: {gl.url}",
-            f"User: {gl.user and gl.user.username}",
+            f"Instance: {instance.get_instance_url()}",
+            f"User: {username}",
             sep="\n",
             style="green bold italic",
         )
