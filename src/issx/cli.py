34b4fd40c213c0b30026c6769e9a6cd7e65d2d99
Szymon Cader <szymon.sc.cader@gmail.com>
2024-04-13 20:47:02 +0200

    feat: Adapt copy command to work with any configured project

--- src/issx/cli.py
+++ src/issx/cli.py
@@ -2,8 +2,8 @@ import asyncio
 from typing import Annotated
 
 import typer
-from gitlab import Gitlab
 from rich.console import Console
+from rich.text import Text
 
 from issx.clients import SupportedBackend
 from issx.clients.gitlab import GitlabClient, GitlabInstanceClient
@@ -15,7 +15,9 @@ app = typer.Typer(no_args_is_help=True)
 
 BackendOption = Annotated[SupportedBackend, typer.Option()]
 InstanceNameOption = Annotated[str, typer.Option("--instance")]
-
+ProjectOption = Annotated[
+    str, typer.Option("--project", help="Project name configured in the config file")
+]
 console = Console()
 
 InstanceManager.register_backend(
@@ -28,23 +30,41 @@ InstanceManager.register_backend(
 
 @app.command()
 def copy(
-    source_project_id: Annotated[int, typer.Option()],
-    target_project_id: Annotated[int, typer.Option()],
+    source_project_name: Annotated[
+        str,
+        typer.Option(
+            "--source", help="Source project name configured in the config file"
+        ),
+    ],
+    target_project_name: Annotated[
+        str,
+        typer.Option(
+            "--target", help="Target project name configured in the config file"
+        ),
+    ],
     issue_id: int,
-    source_backend_name: BackendOption = SupportedBackend.gitlab,
-    target_backend_name: BackendOption = SupportedBackend.gitlab,
 ) -> int:
-    typer.echo(
-        f"Copying issue {issue_id} from project {source_project_id}"
-        f" to project {target_project_id}"
+    console.print(
+        Text.assemble(
+            f"Copying issue {issue_id} from project ",
+            (source_project_name, "bold magenta"),
+            " to project ",
+            (target_project_name, "bold magenta"),
+        )
     )
-    gl = Gitlab.from_config()
-    source_client = GitlabClient(gl, source_project_id)
-    target_client = GitlabClient(gl, target_project_id)
+    config = GenericConfigParser()
+    instance_manager = InstanceManager(config)
+    try:
+        source_client = instance_manager.get_project_client(source_project_name)
+        target_client = instance_manager.get_project_client(target_project_name)
+    except Exception as e:
+        console.print_exception()
+        console.print("Error when configuring client instance.\n", style="red")
+        raise typer.Exit(1) from e
     new_issue = asyncio.run(
         CopyIssueService(source_client, target_client).copy(issue_id)
     )
-    typer.echo(f"New issue created: {new_issue}")
+    console.print(f"Success!\n{new_issue}", style="green")
     return 0
 
 
